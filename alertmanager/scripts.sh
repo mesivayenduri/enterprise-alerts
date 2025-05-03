74cb70bae4b6e1585b7dfaf8c5b7752f5fee06b7
Sasi A <sasidharssan070@gmail.com>
2025-05-03 15:18:53 +0530

    Added All Files

---
+++ alertmanager/scripts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl apply -f alertmanager-deployment.yaml
+kubectl create secret generic alertmanager-development --from-file=./alertmanager.yaml -n development -o yaml --save-config | kubectl apply -n development -f -
