8b22e0d62c2706d3e53d9c26b4618397d23777d8
Sasi A <sasidharssan070@gmail.com>
2025-07-07 18:24:11 +0530

    Added helm installation

--- msteams/scripts.sh
+++ msteams/scripts.sh
@@ -3,6 +3,3 @@
 base64 card.tmpl | tr -d \\n | sed "1s|^|    |" > base64.txt
 cat ./msteams-card-config.yaml ./base64.txt | kubectl apply -n development -f -
 
-kubectl apply -n development -f ./msteams-config.yaml
-
-kubectl apply -n development -f ./prometheus-msteams.yaml
